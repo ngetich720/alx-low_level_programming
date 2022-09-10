8b96a973db0d19024249753da0791306b6a20dde
brian kipchirchir <bkipchirchir720@gmail.com>
2022-09-10 05:34:15 -0700

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,3 +14,12 @@ long li;
 long long lli;
 float f;
 
+print("Size of a char; %ld byte(s)\n", sizeof(c));
+print("Size of an int; %ld byte(s)\n", sizeof(i));
+print("Size of a long int: %ld byte(s)\n", sizeof(li));
+print("Size of a long long: %ld byte(s)\n, sizeof(lli));
+print("Size of a float : %ld byte(s)\n, sizeof(f));
+return (0);
+}
+
+
