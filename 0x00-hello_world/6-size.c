422ea97a62b0177f0e8e2acae32b9c6454c177ae
brian kipchirchir <bkipchirchir720@gmail.com>
2022-09-10 06:14:12 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,9 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function 
+* Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
-* Return: 0 
+* Return: 0
 */
 int main(void)
 {
@@ -14,11 +14,11 @@ long li;
 long long lli;
 float f;
 
-print("Size of a char; %ld byte(s)\n", sizeof(c));
-print("Size of an int; %ld byte(s)\n", sizeof(i));
+print("Size of a char: %ld byte(s)\n", sizeof(c));
+print("Size of an int: %ld byte(s)\n", sizeof(i));
 print("Size of a long int: %ld byte(s)\n", sizeof(li));
-print("Size of a long long: %ld byte(s)\n, sizeof(lli));
-print("Size of a float : %ld byte(s)\n, sizeof(f));
+print("Size of a long long: %ld byte(s)\n", sizeof(lli));
+print("Size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
 
