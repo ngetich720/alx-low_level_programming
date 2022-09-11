cdf146833518adecdaf65700d8e99cd398db624f
brian kipchirchir <bkipchirchir720@gmail.com>
2022-09-11 15:30:08 -0700

    Print comb5 c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,45 +1,35 @@
 #include <stdio.h>
+
 /**
- * main - prints numbers between 012 to 789.
+ * main - Final advanced number arrangement aka comb5
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, e, g;
+	int a, b, c, d;
 
-	i = 48;
-	e = 48;
-	g = 48;
-
-	while (e < 58)
+	for (a = 0; a < 10; ++a)
 	{
-		i = 48;
-		while (i < 58)
+		for (b = 0; b < 10; ++b)
 		{
-			g = 48;
-			while (g < 58)
+			for (c = 0; c < 10; ++c)
 			{
-				if (e != i && e != g && i != g && e  < i && i < g)
+				for (d = 0; d < 10; ++d)
 				{
-					putchar(e);
-					putchar(i);
-					putchar(g);
-					if (i == 56 && e == 55 && g  == 57)
+					if (((a == c) && (b >= d)) || (a > c))
 					{
-						break;
+						continue;
 					}
-					putchar(',');
-					putchar(' ');
 
+					putchar(44);
+					putchar(32);
 				}
-				g++;
 			}
-			i++;
 		}
-		e++;
 	}
-	putchar('\n');
+
+	putchar(10);
 	return (0);
 }
 
