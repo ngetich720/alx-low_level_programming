ceac6ab9209e89d975b94a98855d1ce9af468c36
brian kipchirchir <bkipchirchir720@gmail.com>
2022-09-12 03:56:13 -0700

    print comb5 c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,72 +1,46 @@
 #include <stdio.h>
 
 /**
+ * main - Final advanced number arrangement aka comb5
  *
- *  * main - Final advanced number arrangement aka comb5
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
 
-int main(void)                                                         {
+	for (a = 0; a < 10; ++a)
+	{
+		for (b = 0; b < 10; ++b)
+		{
+			for (c = 0; c < 10; ++c)
+			{
+				for (d = 0; d < 10; ++d)
+				{
+					if (((a == c) && (b >= d)) || (a > c))
+					{
+						continue;
+					}
 
-	        int a, b, c, d;
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(32);
+					putchar(c + '0');
+					putchar(d + '0');
 
-		        for (a = 0; a < 10; ++a)
+					if ((a == 9) && (b == 8) && (c == 9) && (d == 9))
+					{
+						continue;
+					}
 
-				        {
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
 
-						                for (b = 0; b < 10; ++b)
-
-									                {
-
-												                        for (c = 0; c < 10; ++c)
-
-																                        {
-
-																				                                for (d = 0; d < 10; ++d)
-
-																									                                {
-
-																														                                        if (((a == c) && (b >= d)) || (a > c))
-
-																																				                                        {
-
-																																										                                                continue;                                                      }
-
-																																			                                        putchar(a + '0');
-
-																																								                                        putchar(b + '0');
-
-																																													                                        putchar(32);
-
-																																																		                                        putchar(c + '0');
-
-																																																							                                        putchar(d + '0');
-
-																																																												                                        if ((a == 9) && (b == 8) && (c == 9) && (d == 9))
-
-																																																																		                                        {                                                                              continue;
-
-																																																																								                                        }
-
-																																																																	                                        putchar(44);
-
-																																																																						                                        putchar(32);
-
-																																																																											                                }
-
-																								                        }
-
-															                }
-
-								        }
-
-			        putchar(10);
-
-				        return (0);
-
+	putchar(10);
+	return (0);
 }
 
