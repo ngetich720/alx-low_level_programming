d17a9f5be95ae69095d4751a651b23e4efa5dcad
brian kipchirchir <bkipchirchir720@gmail.com>
2022-09-14 09:26:37 -0700

    sign c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,21 +10,21 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-	_putchar(43);
-	return (1);
-	}
-	else if (n < 0)
-	}
-        _putchar(45);
-	return (-1);
-	}
-        else
-        {
-	_putchar(48);
-	return (0);
-	}
-        _putchar('\n');
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+}
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+_putchar('\n');
 }
 
