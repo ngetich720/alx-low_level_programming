044e271399674d33f72715b6e8706a6ba5fe6637
brian kipchirchir <bkipchirchir720@gmail.com>
2022-09-14 08:13:13 -0700

    alphabet c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * print_alphabet - function to print abc
- *
- * Return: 0
+ * print_alphabet - check the code for Holberton School students.
+ * Description: Prints the alphabet with _putchar
+ * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
+        
+        for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
+	        _putchar(c);
 	}
-_putchar('\n');
+        _putchar('\n');
 }
 
