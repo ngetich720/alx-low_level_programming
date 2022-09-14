8c2d1be3906f1d76b847147f17ffcef75488603d
brian kipchirchir <bkipchirchir720@gmail.com>
2022-09-14 09:41:36 -0700

    sign c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,25 @@
 #include "main.h"
 /**
- * print_sign - determines if input number
- * greater, equal or less than zero.
- *
- * @n: input number as integer.
- *
- * Return: if greater than 0 returns 1. if 0 returns 0. if less than 0, -1.
+ * print_sign - function to check for a sign of a number
+ * @n: is the int that will use for the argument of the function
+ * Return: 0
  */
-
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar(43);
-return (1);
+	if (n > 0)
+	{
+	_putchar('+');
+	return(1);
 }
-else if (n < 0)
-}
-_putchar(45);
-return (-1);
-}
-else
-{
-_putchar(48);
-return (0);
-}
-_putchar('\n');
+        else if (n < 0)
+        {
+	_putchar('-')
+	return (-1);
+	}
+        else
+        {
+	_putchar('0');
+	return (0);
+	}
 }
 
