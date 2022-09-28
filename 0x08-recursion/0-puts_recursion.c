adaeb3a975866dc10b01b375548bfa7b5e74101a
ngetich720 <bkipchirchir720@gmail.com>
2022-09-28 08:42:42 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
+ * _puts_recursion - prints a string followed by a new line
  * @s: string to print
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-int _putchar(char c);
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -11,9 +6,8 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _sqrt(int n, int x);
-int is_prime(int n, int start);
-void check(char *s int start, int end, int *flag);
+int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+char *wildcard(char *s3, char *s4);
 
-#endif /* MAIN_h */
+#endif /*MAIN_H*/
