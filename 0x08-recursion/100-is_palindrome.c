6cc317f95a3deaecfeac3e8a96e52ec46ec19555
ngetich720 <bkipchirchir720@gmail.com>
2022-09-28 23:18:43 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,27 +1,51 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string
- * Return: the length of a string.
- */
+* palind2 - obtains length of a
+* @a: string
+* @l: integer to count length
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 
-int _strlen_recursion(char *s)
+int palind2(char *a, int l)
 {
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
 }
+
 /**
- * is_palindrome - detects if a string is a palindrome.
- * @s: string
- * Return: 1 if s is a palindrome, 0 if not.
- */
+* palind3 - compares string vs string reverse
+* @a: string
+* @l: length
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
+
+/**
+* is_palindrome - checks if a string is a palindrome
+* @s: string to evaluate
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
-		return (1);
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+	int l;
+
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
